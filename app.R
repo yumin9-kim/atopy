7f717c718a9a1cb44f6362a707b8ca615d86c720
yumin9-kim <86614936+yumin9-kim@users.noreply.github.com>
2022-04-14 23:48:39 +0900

    Add files via upload

--- app.R
+++ app.R
@@ -63,8 +63,11 @@ ui <- navbarPage("Atopy",
                             sidebarPanel(
                               selectInput("ID_pred", "ID",  unique(zz_fg$ID), unique(zz_fg$ID)[1]),
                               selectInput("Dep_pred", "Dependent variable", varlist$Symptom, "sum_score"),
-                              selectInput("Indep_pred", "Independent variable", varlist[3:4], vars.pred, multiple = T),
-                              radioButtons("Method_pred", "Method", c("ARIMA", "Neural network autoregression"), "ARIMA", inline = T),
+                              selectInput("Indep_pred", "Independent variable", varlist[3:4], setdiff(vars.pred, c("season", "dow")), multiple = T),
+                              radioButtons("Method_pred", "Method", c("ARIMA", "Prophet"), "ARIMA", inline = T),
+                              h4("Only for prophet model"),
+                              textInput("changepoint", h5("Changepoint prior scale: (0.001 ~ 0.5)"), 0.05),
+                              textInput("seasonality", h5("Seasonality prior scale (0.01 ~ 10)"), 10),
                               actionButton("action_pred", "Run prediction")
                             ),
                             mainPanel(
@@ -82,36 +85,7 @@ ui <- navbarPage("Atopy",
                                                    withLoader(DTOutput("predtable", width = "100%"), type="html", loader="loader6")
                                           ),
                                           tabPanel("Likelihood", 
-                                                   verbatimTextOutput("predres"))
-                              )
-                            )
-                            
-                            
-                            
-                          )
-                          
-                 ),
-                 tabPanel("Individual: prophet",
-                          sidebarLayout(
-                            sidebarPanel(
-                              selectInput("ID_pred2", "ID",  unique(zz_fg$ID), unique(zz_fg$ID)[1]),
-                              selectInput("Dep_pred2", "Dependent variable", varlist$Symptom, "sum_score"),
-                              selectInput("Indep_pred2", "Independent variable", setdiff(unlist(varlist[3:4]), c("season", "dow")), setdiff(vars.pred, c("season", "dow")), multiple = T),
-                              textInput("changepoint", "Changepoint prior scale (0.001 ~ 0.5)", 0.05),
-                              textInput("seasonality", "Seasonality prior scale (0.01 ~ 10)", 10),
-                              actionButton("action_pred2", "Run prediction")
-                            ),
-                            mainPanel(
-                              withLoader(plotOutput("prophetplot", width = "100%"), type="html", loader="loader6"),
-                              h3("Download options"),
-                              wellPanel(
-                                uiOutput("downloadControls_prophet"),
-                                downloadButton("downloadButton_prophet", label = "Download the plot")
-                              ),
-                              tabsetPanel(type = "pills",
-                                          tabPanel("Fitted value", 
-                                                   withLoader(DTOutput("predtable2", width = "100%"), type="html", loader="loader6")
-                                          ),
+                                                   verbatimTextOutput("predres")),
                                           tabPanel("Prophet components plot", 
                                                    withLoader(plotOutput("prophetcomp", width = "100%"), type="html", loader="loader6"),
                                           )
@@ -185,91 +159,109 @@ server <- function(input, output, session) {
         model(ARIMA(as.formula(paste0(input$Dep_pred, " ~ ", paste(input$Indep_pred, collapse = "+")))))
     }
     
-    
-    return(fit)
-  })
-  
-  obj.prophet <- eventReactive(input$action_pred2, {
-    data <- dplyr::filter(tibble(zz), ID == input$ID_pred2) %>%
-            dplyr::mutate(ds=date, y=sum_score, cap = 24, floor = 0) %>% 
-            dplyr::select(c(date, ds, y, input$Dep_pred2, input$Indep_pred2)) %>% na.omit()
-            
-    mod <- prophet(yearly.seasonality = T, changepoint.prior.scale = as.numeric(input$changepoint), seasonality.prior.scale = as.numeric(input$seasonality))
-    
-    for (i in input$Indep_pred2){
-      mod <- mod %>% add_regressor(i)
+    if (input$Method_pred == "Prophet"){
+      data <- dplyr::filter(tibble(zz), ID == input$ID_pred) %>%
+        dplyr::mutate(ds=date, y=sum_score, cap = 24, floor = 0)
+      
+      vars.pred <- setdiff(input$Indep_pred, c("season", "dow"))
+      data2 <- data %>% dplyr::select(c(date, ds, y, input$Dep_pred, vars.pred)) %>% na.omit()
+      
+      mod <- prophet(yearly.seasonality = T, changepoint.prior.scale = as.numeric(input$changepoint), seasonality.prior.scale = as.numeric(input$seasonality))
+      for (i in vars.pred){
+        mod <- mod %>% add_regressor(i)
+      }
+      fit <- mod %>% fit.prophet(data2)
     }
-    fit <- mod %>% fit.prophet(data)
     
     return(fit)
-    
   })
   
+  obj.predtable <- reactive({
+    if (input$Method_pred == "ARIMA"){
+      fit <- obj.ind()
+      
+      res.ARIMA <- data.table(dplyr::bind_cols(fitted(fit), sum_score = lapply(1:nrow(fit), function(x){
+        fit[, 2][[1]][[1]]$data$sum_score
+      }) %>% Reduce(c, .)))
+      
+      table <- datatable(res.ARIMA[, -c(1, 2)], rownames=F, extensions = "Buttons", caption = "Individual data",
+                         options = c(jstable::opt.data(input$ID_pred),
+                                     
+                                     list(scrollX = TRUE)
+                         )
+      )}
+    
+    else if (input$Method_pred == "Prophet"){
+      mod <- obj.ind()
+      pred <- predict(mod)
+      tab <- data.table(dplyr::bind_cols(date = ymd(pred$ds), predict = pred$yhat, sum_score = mod$history$sum_score))
+      tab.prophet <- tab %>% merge(data.table(dplyr::filter(tibble(zz), ID == input$ID_pred))[, .(date)], by = "date", all.y = T) %>% .[order(date)] %>% 
+        as_tsibble() %>% fill_gaps() %>% as.data.table()
+      RMSE <- tab %>% summarize(rmse = mean((predict-sum_score)^2)) %>% round(., 3)
+      table <- datatable(tab.prophet, rownames=F, extensions = "Buttons", caption = paste0("RMSE = ", RMSE),
+                         options = c(jstable::opt.data(input$ID_pred),
+                                     
+                                     list(scrollX = TRUE)
+                         )
+    )}
+    return(table)
+  })
   
   output$predtable <- renderDT({
-    fit <- obj.ind()
-    
-    res.ARIMA <- data.table(dplyr::bind_cols(fitted(fit), sum_score = lapply(1:nrow(fit), function(x){
-      fit[, 2][[1]][[1]]$data$sum_score
-    }) %>% Reduce(c, .)))
-    
-    datatable(res.ARIMA[, -c(1, 2)], rownames=F, extensions = "Buttons", caption = "Individual data",
-              options = c(jstable::opt.data(input$ID_pred),
-                          
-                          list(scrollX = TRUE)
-              )
-    )
-  })
-
-  output$predtable2 <- renderDT({
-    mod <- obj.prophet()
-    pred <- predict(mod)
-    data.table(dplyr::bind_cols(date = ymd(pred$ds), predict = pred$yhat, sum_score = mod$history$sum_score))
+    obj.predtable()
   })
   
   output$predest <- renderDT({
-    fit <- obj.ind()
-    datatable(fit[, 2][[1]][[1]]$fit$par, rownames = F, extensions = "Buttons", caption = "Result",
-              options = c(jstable::opt.tbreg(input$ID_pred),
-                          
-                          list(scrollX = TRUE)
-              )
-    ) %>% formatRound(columns=2:5, digits=3)
-  })
-  
-  output$prophetcomp <- renderPlot({
-    mod <- obj.prophet()
-    pred <- predict(mod)
-    prophet_plot_components(mod2, pred)
+    if (input$Method_pred == "ARIMA"){
+      fit <- obj.ind()
+      datatable(fit[, 2][[1]][[1]]$fit$par, rownames = F, extensions = "Buttons", caption = "Result",
+                options = c(jstable::opt.tbreg(input$ID_pred),
+                            
+                            list(scrollX = TRUE)
+                )
+      ) %>% formatRound(columns=2:5, digits=3)
+    } else{
+      
+    }
   })
   
   output$predres <- renderPrint({
-    obj.ind()[, 2][[1]][[1]]$fit$fit
+    if (input$Method_pred == "ARIMA"){
+      obj.ind()[, 2][[1]][[1]]$fit$fit
+    }
   })
   
+  output$prophetcomp <- renderPlot({
+    if (input$Method_pred == "Prophet"){
+      mod <- obj.ind()
+      pred <- predict(mod)
+      prophet_plot_components(mod, pred)}
+  })
   
   obj.predplot <- reactive({
-    fit <- obj.ind()
+    if (input$Method_pred == "ARIMA"){
+      fit <- obj.ind()
+      
+      res.ARIMA <- data.table(dplyr::bind_cols(fitted(fit), sum_score = lapply(1:nrow(fit), function(x){
+        fit[, 2][[1]][[1]]$data$sum_score
+      }) %>% Reduce(c, .))) %>% melt(idvars = "ID", measure.vars = c(".fitted", "sum_score"))
+      
+      
+      ggpubr::ggline(res.ARIMA[ID == input$ID_pred], "date", "value", color = "variable", plot_type  = "l", xlab = "", ylab = "sum_score")
+    } else if (input$Method_pred == "Prophet"){
+      mod <- obj.ind()
+      pred <- predict(mod)
+      
+      tab.prophet <- data.table(dplyr::bind_cols(date = as.Date(pred$ds), predict = pred$yhat, sum_score = mod$history$sum_score)) %>% 
+        merge(data.table(dplyr::filter(tibble(zz), ID == input$ID_pred))[, .(date)], by = "date", all.y = T) %>% 
+        as_tsibble() %>% fill_gaps() %>% as.data.table() %>%
+        melt(measure.vars = c("predict", "sum_score"))
+      
+      ggpubr::ggline(tab.prophet, "date", "value", color = "variable", plot_type  = "l", xlab = "", ylab = "sum_score")
+    }
     
-    res.ARIMA <- data.table(dplyr::bind_cols(fitted(fit), sum_score = lapply(1:nrow(fit), function(x){
-      fit[, 2][[1]][[1]]$data$sum_score
-    }) %>% Reduce(c, .))) %>% melt(idvars = "ID", measure.vars = c(".fitted", "sum_score"))
     
-    
-    ggpubr::ggline(res.ARIMA[ID == input$ID_pred], "date", "value", color = "variable", plot_type  = "l", xlab = "", ylab = "sum_score")
   })
-  
-  
-  obj.prophetplot <- reactive({
-    mod <- obj.prophet()
-    pred <- predict(mod)
-    
-    tab.prophet <- data.table(dplyr::bind_cols(date = pred$ds, predict = pred$yhat, sum_score = mod$history$sum_score)) %>%
-      melt(measure.vars = c("predict", "sum_score"))
-    
-    ggpubr::ggline(tab.prophet, "date", "value", color = "variable", plot_type  = "l", xlab = "", ylab = "sum_score")
-  })
-  
   
   output$downloadControls_kap <- renderUI({
     fluidRow(
@@ -292,36 +284,8 @@ server <- function(input, output, session) {
     )
   })
   
-  
-  output$downloadControls_prophet <- renderUI({
-    fluidRow(
-      column(4,
-             selectizeInput("kap_file_ext2", "File extension (dpi = 300)", 
-                            choices = c("jpg","pdf", "tiff", "svg", "pptx"), multiple = F, 
-                            selected = "pptx"
-             )
-      ),
-      column(4,
-             sliderInput("fig_width_kap2", "Width (in):",
-                         min = 5, max = 20, value = 10
-             )
-      ),
-      column(4,
-             sliderInput("fig_height_kap2", "Height (in):",
-                         min = 5, max = 20, value = 6
-             )
-      )
-    )
-  })
-  
   output$predplot <- renderPlot({
     obj.predplot()
-    
-  })
-  
-  
-  output$prophetplot <- renderPlot({
-    obj.prophetplot()
     
   })
   
@@ -347,35 +311,6 @@ server <- function(input, output, session) {
                        
                      } else{
                        ggsave(file, obj.km(), dpi = 300, units = "in", width = input$fig_width_kap, height =input$fig_height_kap)
-                     }
-                     
-                   })
-      
-      
-    })
-  
-  output$downloadButton_prophet <- downloadHandler(
-    filename =  function() {
-      paste(input$ID_pred2, "_prophet_plot.", input$kap_file_ext2 ,sep="")
-    },
-    # content is a function with argument file. content writes the plot to the device
-    content = function(file) {
-      withProgress(message = 'Download in progress',
-                   detail = 'This may take a while...', value = 0, {
-                     for (i in 1:15) {
-                       incProgress(1/15)
-                       Sys.sleep(0.01)
-                     }
-                     
-                     if (input$kap_file_ext2 == "pptx"){
-                       my_vec_graph <- rvg::dml(ggobj  = obj.prophetplot())
-                       doc <- officer::read_pptx()
-                       doc <- officer::add_slide(doc, layout = "Title and Content", master = "Office Theme")
-                       doc <- officer::ph_with(doc, my_vec_graph, location = officer::ph_location(width = input$fig_width_kap2, height = input$fig_height_kap2))
-                       print(doc, target = file)
-                       
-                     } else{
-                       ggsave(file, obj.km(), dpi = 300, units = "in", width = input$fig_width_kap2, height =input$fig_height_kap2)
                      }
                      
                    })
